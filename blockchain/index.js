500d3ee14396f2078254f8e75bff15d6d586dcb9
jmc <jmcreat@naver.com>
2020-01-02 10:49:19 +0900

    first

---
+++ blockchain/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//시작은 yarn start
+const name = "jmc", age = 31, gender = "man";
+//gender? 물음표 붙이면 선택 가능
+const sayHi = (name, age, gender) => {
+    console.log('${name}');
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ blockchain/index.ts
@@ -0,0 +1,12 @@
+//시작은 yarn start
+//export{} 붙여 줘야 한다.
+const name ="jmc",
+  age = 31,
+  gender = "man";
+//gender? 물음표 붙이면 선택 가능
+const sayHi = (name, age, gender)=>{
+ console.log('${name}');
+};
+sayHi(name,age,gender);
+
+export{};
